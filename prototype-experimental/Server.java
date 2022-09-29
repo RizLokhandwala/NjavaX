1a7ae0256fadd488931b7c39c6e8198e3eca2160
Glenntu <glenntu@live.com>
2022-09-28 21:42:52 -0500

    add server.java

---
+++ prototype-experimental/Server.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package MHTTPServer;
+
+/**
+ * This code is from
+ * @author Mateusz Jarzyna
+ * Read the full article https://dev.to/mateuszjarzyna/build-your-own-http-server-in-java-in-less-than-one-hour-only-get-method-2k02
+ *
+ * GAT -- I have successfully connected to a browser but do not have the file service working yet
+ */
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+// Read the full article https://dev.to/mateuszjarzyna/build-your-own-http-server-in-java-in-less-than-one-hour-only-get-method-2k02
+public class Server {
+
+    public static void main( String[] args ) throws Exception {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            while (true) {
+                try (Socket client = serverSocket.accept()) {
+                    handleClient(client);
+                }
+            }
+        }
+    }
+
+    private static void handleClient(Socket client) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+        StringBuilder requestBuilder = new StringBuilder();
+        String line;
+        while (!(line = br.readLine()).isBlank()) {
+            requestBuilder.append(line + "\r\n");
+        }
+
+        String request = requestBuilder.toString();
+        String[] requestsLines = request.split("\r\n");
+        String[] requestLine = requestsLines[0].split(" ");
+        String method = requestLine[0];
+        String path = requestLine[1];
+        String version = requestLine[2];
+        String host = requestsLines[1].split(" ")[1];
+
+        List<String> headers = new ArrayList<>();
+        for (int h = 2; h < requestsLines.length; h++) {
+            String header = requestsLines[h];
+            headers.add(header);
+        }
+
+        String accessLog = String.format("==>Client %s, method %s, path %s, version %s, host %s, headers %s",
+                client.toString(), method, path, version, host, headers.toString());
+        System.out.println(accessLog);
+
+
+        Path filePath = getFilePath(path);
+        if (Files.exists(filePath)) {
+            // file exist
+            String contentType = guessContentType(filePath);
+            sendResponse(client, "200 OK", contentType, Files.readAllBytes(filePath));
+        } else {
+            // 404
+            byte[] notFoundContent = "<h1>File Not found :(</h1>".getBytes();
+            sendResponse(client, "404 Not Found", "text/html", notFoundContent);
+        }
+
+    }
+
+    private static void sendResponse(Socket client, String status, String contentType, byte[] content) throws IOException {
+        OutputStream clientOutput = client.getOutputStream();
+        clientOutput.write(("HTTP/1.1 \r\n" + status).getBytes());
+        clientOutput.write(("ContentType: " + contentType + "\r\n").getBytes());
+        clientOutput.write("\r\n".getBytes());
+        clientOutput.write(content);
+        clientOutput.write("\r\n\r\n".getBytes());
+        clientOutput.flush();
+        client.close();
+    }
+
+    private static Path getFilePath(String path) {
+        if ("/".equals(path)) {
+            path = "/index.html";
+        }
+
+        return Paths.get("/tmp/www", path);
+    }
+
+    private static String guessContentType(Path filePath) throws IOException {
+        return Files.probeContentType(filePath);
+    }
+
+}
--- prototype-experimental/readme.md
+++ prototype-experimental/readme.md
@@ -1,1 +1,2 @@
-This will describe the files in this folder
+##This will describe the files in this folder
+ - Server.java is a simple TCP connection to port 8080
