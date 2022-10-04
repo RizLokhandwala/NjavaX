e311627e3ae713103e67bbf7f91df61474544820
Glenntu <glenntu@live.com>
2022-10-03 23:11:37 -0500

    new code

---
+++ prototype-experimental/Client-ServerPair/Server.java
@@ -0,0 +1,112 @@
+package Server;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main(String[] args)
+    {
+        ServerSocket server = null;
+  
+        try {
+  
+            // server is listening on port 8080
+            server = new ServerSocket(8080);
+            //server.setReuseAddress(true);
+  
+            // running infinite loop for getting
+            // client request
+            while (true) {
+  
+                // socket object to receive incoming client
+                // requests
+            	System.out.println("before accept");
+                Socket client = server.accept();
+  
+                // Displaying that new client is connected
+                // to server
+                System.out.println("New client connected"
+                                   + client.getInetAddress()
+                                         .getHostAddress());
+  
+                // create a new thread object
+                ClientHandler clientSock
+                    = new ClientHandler(client);
+  
+                // This thread will handle the client
+                // separately
+                new Thread(clientSock).start();
+            }
+        }
+        catch (IOException e) {
+        	System.out.println("Exception: ");
+            e.printStackTrace();
+        }
+        finally {
+            if (server != null) {
+                try {
+                    server.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+  
+    // ClientHandler class
+    private static class ClientHandler implements Runnable {
+        private final Socket clientSocket;
+  
+        // Constructor
+        public ClientHandler(Socket socket)
+        {
+            this.clientSocket = socket;
+        }
+  
+        public void run()
+        {
+            PrintWriter out = null;
+            BufferedReader in = null;
+            try {
+                    
+                  // get the outputstream of client
+                out = new PrintWriter(
+                    clientSocket.getOutputStream(), true);
+  
+                  // get the inputstream of client
+                in = new BufferedReader(
+                    new InputStreamReader(
+                        clientSocket.getInputStream()));
+  
+                String line;
+                while ((line = in.readLine()) != null) {
+  
+                    // writing the received message from
+                    // client
+                    System.out.printf(
+                        " Sent from the client: %s\n",
+                        line);
+                    out.println(line);
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+                    if (in != null) {
+                        in.close();
+                        clientSocket.close();
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
