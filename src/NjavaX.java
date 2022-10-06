b52bc0546986e3d292cab0db1b6d667017522141
Glenn Turner <glenntu@live.com>
2022-10-05 21:45:17 -0500

    Create NjavaX.java
    Created src folder and NjavaX main class file

---
+++ src/NjavaX.java
@@ -0,0 +1,59 @@
+/**
+ * This code is from
+ * @author Mateusz Jarzyna
+ * Read the full article https://dev.to/mateuszjarzyna/build-your-own-http-server-in-java-in-less-than-one-hour-only-get-method-2k02
+ *
+ * GAT -- I have made this multithreaded
+ */
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class NjavaX {
+public static void main(String[] args) {
+        try {
+
+            // server is listening on port 8080
+            server = new ServerSocket(8080);
+            // server.setReuseAddress(true);
+
+            // running infinite loop for getting
+            // client request
+            while (true) {
+
+                // socket object to receive incoming client
+                // requests
+                System.out.println("before accept");
+                Socket client = server.accept();
+
+                // Displaying that new client is connected
+                // to server
+                System.out.println("New client connected"
+                        + client.getInetAddress()
+                                .getHostAddress());
+
+                // create a new thread object
+                ClientHandler clientSock = new ClientHandler(client);
+
+                // This thread will handle the client
+                // separately
+                new Thread(clientSock).start();
+            }
+        } catch (IOException e) {
+            System.out.println("Exception: ");
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+  
+  
+  
+}
