05ca6db94f403928203d45015bb349d109f874b9
Shn9 <53298173+SeanHungNguyen@users.noreply.github.com>
2022-11-16 16:14:46 -0600

    Update ServerInfo.java

--- unittest/ServerInfo.java
+++ unittest/ServerInfo.java
@@ -1,73 +1,63 @@
 import java.io.*;
 import java.util.*;
 
-class Test1{
+class ServerInfo{
+    static int index;
+    
+     //public static void main(String[] args)
+    //{
+        ////avaiable instances
+        //GlobalInfo instance = GlobalInfo.getInstance();
+     
+        //instance.addServerEntry("localhost", 8080);
+        //instance.addServerEntry("127.12.12.1", 8080);
+        //instance.addServerEntry("192.120.6.1", 8081);
 
-     public static void main(String[] args){
+        //ServerInfo.returnInfo(index);
+        //index = changeIndex(index);
+        //System.out.printf("Index changed to %s\n", index);
+        
+    //}
 
-        //avaiable instances
-        GlobalInfo instance = GlobalInfo.getInstance();
-        instance.addServerEntry("localhost", 8080);
-        instance.addServerEntry("127.12.12.1", 8080);
-        instance.addServerEntry("192.120.6.1", 8081);
-        int index = 0;
-        
-        Test1.returnInfo(index);
-        int x = changeIndex(index);
-        //System.out.printf("Index changed to %s\n",x[0]);
-        //System.out.printf("Index changed to %s\n",x[1]);
-        
-    }
+   
 
     public static List returnInfo(int a)
     {
-        //Get host and port info
+        //Return IP and port info
         GlobalInfo instance = GlobalInfo.getInstance();
-
         List<Object> info = instance.getEntry(a);
+         
         System.out.printf("Returned %s\n",info.get(0));
         System.out.printf("Returned %s\n",info.get(1));
-
         return info;
 
      }
      
+     
+     
      public static int changeIndex(int a)
      {
-         //changes hostname and IP
+        //increments index or sets index to 0 if index is at last server entry
         GlobalInfo instance = GlobalInfo.getInstance();
         int num = instance.getNumServerEntries();
-        //System.out.printf("Returned %s\n",num);
+        num = num - 1;
+        //System.out.printf("Printed %s\n",num);
+      
         int index = a;
-         if(a < num)
+          
+         if(a == num)
          {
-            index += 1;
+            index = 0;
             System.out.printf("Returned %s\n",index);
             return index;
          }
          else
          {
-            index = 0;
+            index += 1;
+            System.out.printf("Returned %s\n",index);
             return index;
          }
      }
 
-    // public static int[] changeIndex(int a, int b, int[] info)
-    // {
-    //     //if not current server, increment index
-    //     GlobalInfo instance = GlobalInfo.getInstance();
-    //     int num = instance.getNumServerEntries();
-    //     num = num * 2;
-    //     System.out.printf("Returned %s\n",num);
-
-    //     int[] serverInfo = new int[num];
-    //     serverInfo[0] = a + 1;
-    //     info[a] = serverInfo[0];
-    //     serverInfo[1] = b + 1;
-    //     info[b] = serverInfo[1];
-    //     System.out.printf("Returned %s\n",serverInfo[0]);
-    //     System.out.printf("Returned %s\n",serverInfo[1]);
-    //     return serverInfo;
-    // }
-     
 }
+
